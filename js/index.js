34ffda4ad99281786c5671381a7765055c7ced16
Andriy <andriy.kostretskiy@gmail.com>
2018-10-01 13:27:07 +0000

    Update

--- js/index.js
+++ js/index.js
@@ -1,25 +1,23 @@
 const quadraticСoefficient = parseFloat(prompt('Enter quadratic coefficient A > 0'));
 const linearСoefficient = parseFloat(prompt('Enter linear coefficient B > 0'));
-const freeConsnant = parseFloat(prompt('Enter free consnant C > 0'));
-const solution = solveQuadraticEquation(quadraticСoefficient, linearСoefficient, freeConsnant);
+const freeConstant = parseFloat(prompt('Enter free consnant C > 0'));
+const solution = solveQuadraticEquation(quadraticСoefficient, linearСoefficient, freeConstant);
 alert(solution);
 
-function solveQuadraticEquation(quadraticСoefficient, linearСoefficient, freeConsnant) {
-    const descriminant = calcDiscriminant(quadraticСoefficient,linearСoefficient,freeConsnant);
+function solveQuadraticEquation(quadraticСoefficient, linearСoefficient, freeConstant) {
+    const descriminant = calcDiscriminant(quadraticСoefficient,linearСoefficient,freeConstant);
     if(descriminant > 0) {
-        const root1 = (-linearСoefficient + Math.sqrt(descriminant))/2*quadraticСoefficient;
-        const root2 = (-linearСoefficient - Math.sqrt(descriminant))/2*quadraticСoefficient;
+        const root1 = (-linearСoefficient + Math.sqrt(descriminant))/(2*quadraticСoefficient);
+        const root2 = (-linearСoefficient - Math.sqrt(descriminant))/(2*quadraticСoefficient);
         return 'Root1 = ' + root1 + '  Root2 = ' + root2;
     } else if(descriminant === 0) {
         const root = -linearСoefficient/(2*quadraticСoefficient);
         return 'Root = ' + root;
-    } else {
-        return 'No solutions';
-    }
+    }  return 'No solutions';
 }
 
-function calcDiscriminant(quadraticСoefficient, linearСoefficient, freeConsnant) {
-    const descriminant = Math.pow(linearСoefficient, 2) - 4*quadraticСoefficient*freeConsnant;
+function calcDiscriminant(quadraticСoefficient, linearСoefficient, freeConstant) {
+    const descriminant = Math.pow(linearСoefficient, 2) - 4*quadraticСoefficient*freeConstant;
     return descriminant;
 }
 
